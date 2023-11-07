5f737ffa3aa8bc861304a47b717760a45ac93898
cjoshua <cjoshua@chalmers.se>
2023-11-07 22:32:51 +0100

    #1 add validation

--- server/src/models/booking.js
+++ server/src/models/booking.js
@@ -4,10 +4,14 @@ const Schema = mongoose.Schema;
 const bookingSchema = new Schema({
     patientID: { type: Schema.Types.ObjectId, ref: 'Patient' },
     dentistID: { type: Schema.Types.ObjectId, ref: 'Dentist' },
-    dentistName: String,
-    patientName: String,
-    date: String,
-    time: String,
+    dentistName: { type: String, required: true },
+    patientName: { type: String, required: true },
+    date: {
+        type: String,
+        required: true,
+        match: [/^\d{4}-\d{2}-\d{2}$/, 'is not a valid date format']
+    },
+    time: { type: String, required: true },
     message: String
 });
 
